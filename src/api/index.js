e86e9f488b921032eb7da4845849579b4890a9be
tsadik <findtsadik@gmail.com>
2019-07-25 04:10:12 +0300

    :see_no_evil: update gitignore file and move API_ROOT to env

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import UserAccountAPI from './user-account.api';
 // eslint-disable-next-line import/no-cycle
 import PageAPI from './page.api';
 
-const API_ROOT = 'http://localhost:3000/api';
+const API_ROOT = process.env.VUE_APP_API_ROOT;
 // eslint-disable-next-line
 export { 
   API_ROOT,
